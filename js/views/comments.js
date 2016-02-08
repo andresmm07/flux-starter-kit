1dc7106dc8f0d1b0d28ade18e848c39b47516833
Andres <andres.mm07@gmail.com>
2016-02-08 11:40:08 -0500

    finished flux intro tutorial

--- js/views/comments.js
+++ js/views/comments.js
@@ -1,13 +1,45 @@
 var React = require('react');
+var CommentStore = require('../stores/comment-store');
+
+function getStateFromCommentStore() {
+  return {
+    comments: CommentStore.getAll()
+  }
+}
 
 var Comments = React.createClass({
 
+  onChange: function() {
+    this.setState(getStateFromCommentStore());
+  },
+
+  getInitialState: function() {
+    return getStateFromCommentStore();
+  },
+
+  componentDidMount: function() {
+    CommentStore.addChangeListener(this.onChange);
+  },
+
+  componentWillUnmount: function() {
+    CommentStore.removeChangeListener(this.onChange);
+  },
+
   render: function() {
+    var comments = this.state.comments.map(function(comment, index){
+      return (
+        <div className='comment' key={'comment-' + index}>
+          {comment.text}
+        </div>
+      )
+    });
+
     return (
-      <div />
+      <div className='comments'>
+        {comments}
+      </div>
     );
   }
-
 });
 
 module.exports = Comments;
